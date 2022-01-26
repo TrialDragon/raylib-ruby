9b71781fda64b4ba4845e2dcfcbdf6df25669c3e
TrialDragon <cj.2003@live.com>
2022-01-25 23:57:21 -0800

    Add color initialization method
    Added struct field accessors and initialization method for ease of use.
    
    Need to quickly intialize a color with all the values at once rather
    than intialize each field by itself.
    
    May be a bit hacky as super is not called; super causes errors and
    depcreation message as FFI::Struct uses it for layout whilst we use it
    for intializing fields.
    
    Alternative is a new_<struct_name> method. E.g. new_color. The debuff of
    this is that it is non-standardized.

--- lib/structs/color.rb
+++ lib/structs/color.rb
@@ -1,10 +1,50 @@
 # frozen_string_literal: true
 
+require 'ffi'
+
 module Raylib
   class Color < FFI::Struct
     layout :r, :uchar,
            :g, :uchar,
            :b, :uchar,
            :a, :uchar
+    def r
+      self[:r]
+    end
+
+    def g
+      self[:g]
+    end
+
+    def b
+      self[:b]
+    end
+
+    def a
+      self[:a]
+    end
+
+    def r=(val)
+      self[:r] = val
+    end
+
+    def g=(val)
+      self[:g] = val
+    end
+
+    def b=(val)
+      self[:b] = val
+    end
+
+    def a=(val)
+      self[:a] = val
+    end
+
+    def initialize(red, green, blue, alpha)
+      self.r = red
+      self.g = green
+      self.b = blue
+      self.a = alpha
+    end
   end
 end
