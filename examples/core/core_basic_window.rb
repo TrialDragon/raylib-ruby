2f517bd068b54ec41e43d30f8c3f7754a28a0c8b
TrialDragon <cj.2003@live.com>
2022-01-26 23:41:04 -0800

    Update basic window example
    The example now includes references to the submodules which functions
    will be kept in as well as the basis for how 'Begin' and 'End' functions
    should be handled; by blocks.

--- examples/core/core_basic_window.rb
+++ examples/core/core_basic_window.rb
@@ -10,14 +10,11 @@ Raylib::Core.init_window SCREEN_WIDTH, SCREEN_HEIGHT, 'raylib [core] example - b
 
 Raylib::Core.set_target_fps 60
 
-unless Raylib::Core.window_should_close?
-  Raylib::Core.begin_drawing
-
-  Raylib::Core.clear_background Raylib::RAYWHITE
-
-  Raylib::Core.draw_text 'Congrats! You created your first window!', 190, 200, 20, Raylib::LIGHTGRAY
-
-  Raylib::Core.end_drawing
+until Raylib::Core.window_should_close
+  Raylib::Core.draw do
+    Raylib::Core.clear_background Raylib::RAYWHITE
+    Raylib::Text.draw_text 'Congrats! You created your first window!', 190, 200, 20, Raylib::LIGHTGRAY
+  end
 end
 
 Raylib::Core.close_window
